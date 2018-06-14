1c46213a30ce23ceae6fb080e911fa34ef504779
ArdamWeng <ardam91931@gmail.com>
2018-06-14 10:34:12 +0800

    修正:xml編譯失敗

--- app/src/main/java/tw/edu/niu/googlelogin/MoneyActivity.java
+++ app/src/main/java/tw/edu/niu/googlelogin/MoneyActivity.java
@@ -1,76 +1,88 @@
 package tw.edu.niu.googlelogin;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 import tw.edu.niu.googlelogin.model.City;
 
 public class MoneyActivity extends AppCompatActivity {
     final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    ListView listView;
+    Button btntomain;
+    ArrayList<String> mArrayList = new ArrayList<String>();
+    ArrayList<String> mArrayListuserID = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money);
-        CollectionReference cities = db.collection("cities");
-
-        Map<String, Object> data1 = new HashMap<>();
-        data1.put("name", "San Francisco");
-        data1.put("state", "CA");
-        data1.put("country", "USA");
-        data1.put("capital", false);
-        data1.put("population", 860000);
-        cities.document("SF").set(data1);
-
-        Map<String, Object> data2 = new HashMap<>();
-        data2.put("name", "Los Angeles");
-        data2.put("state", "CA");
-        data2.put("country", "USA");
-        data2.put("capital", false);
-        data2.put("population", 3900000);
-        cities.document("LA").set(data2);
-
-        Map<String, Object> data3 = new HashMap<>();
-        data3.put("name", "Washington D.C.");
-        data3.put("state", null);
-        data3.put("country", "USA");
-        data3.put("capital", true);
-        data3.put("population", 680000);
-        cities.document("DC").set(data3);
-
-        Map<String, Object> data4 = new HashMap<>();
-        data4.put("name", "Tokyo");
-        data4.put("state", null);
-        data4.put("country", "Japan");
-        data4.put("capital", true);
-        data4.put("population", 9000000);
-        cities.document("TOK").set(data4);
-
-        Map<String, Object> data5 = new HashMap<>();
-        data5.put("name", "Beijing");
-        data5.put("state", null);
-        data5.put("country", "China");
-        data5.put("capital", true);
-        data5.put("population", 21500000);
-        cities.document("BJ").set(data5);
-
-
-        DocumentReference docRef = db.collection("cities").document("BJ");
-        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        btntomain = findViewById(R.id.btntomain);
+        listView = findViewById(R.id.listview);
+        Query query = db.collection("users").whereEqualTo("hasfee",false);
+        query.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                City city = documentSnapshot.toObject(City.class);
-                Log.d("mytag", city.getCountry() + " => " + city.getName());
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if(e != null){
+                    Log.e("mylog" , "Error", e);
+                }else{
+                    Log.e("mylog", queryDocumentSnapshots.size()+"");
+                    if(queryDocumentSnapshots == null || queryDocumentSnapshots.isEmpty()){
+                        AlertDialog.Builder bdr=new AlertDialog.Builder(MoneyActivity.this);
+                        bdr.setMessage("無成員");
+                        bdr.setTitle("目前無任何成員");
+                        bdr.setIcon(android.R.drawable.ic_dialog_info);
+                    }else {
+                        Log.e("mylog", "ENTER DocumentSnapshot");
+                        DocumentSnapshot documentSnapshot = null;
+                        for (DocumentSnapshot snapshot : queryDocumentSnapshots) {
+                            documentSnapshot = snapshot;
+                            mArrayList.add(snapshot.get("name").toString());
+                            mArrayListuserID.add(snapshot.getId());
+                            Log.e("mylog", snapshot.get("name").toString());
+                        }
+                        ArrayAdapter adapter = new ArrayAdapter(MoneyActivity.this,
+                                android.R.layout.simple_list_item_1,
+                                mArrayList);
+                        listView.setAdapter(adapter);
+                    }
+                }
             }
         });
+
+        btntomain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MoneyActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        
+
     }
 }
