b4cd960e8ae0cb26382a72e21bf8bbf4896fdc00
ArdamWeng <ardam91931@gmail.com>
2018-06-19 02:10:13 +0800

    解決動畫圖片過大 crash 的問題

--- app/src/main/java/tw/edu/niu/googlelogin/DataActivity.java
+++ app/src/main/java/tw/edu/niu/googlelogin/DataActivity.java
@@ -1,30 +1,43 @@
 package tw.edu.niu.googlelogin;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import static tw.edu.niu.googlelogin.R.drawable.animation;
 
 public class DataActivity extends AppCompatActivity {
     ImageView imageView;
+
     AnimationDrawable animationDrawable;
+    Button btnDataback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
-
+        btnDataback = findViewById(R.id.btnDataback);
         imageView  =  findViewById(R.id.imageView_animation);
-        imageView.setBackgroundResource(animation);
+        imageView.setBackgroundResource(R.drawable.ang);
+        //mTextView.setBackground(mFrameAnimation);
+        //mFrameAnimation.start();
+        //imageView.setBackgroundResource(R.drawable.animation);
         animationDrawable = (AnimationDrawable) imageView.getBackground();
-
-        imageView.post(new Runnable() {
+        animationDrawable.start();
+        btnDataback.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run()  {
-                animationDrawable.start();
+            public void onClick(View v) {
+                Intent intent = new Intent(DataActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
+
+
     }
 }
